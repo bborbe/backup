b79dbd4c0d70f8520864a74f264175b1c15ccc85
Benjamin Borbe <bborbe@rocketnews.de>
2013-08-22 21:21:54 +0200

    implement list backups

--- service/backup_service.go
+++ service/backup_service.go
@@ -1,12 +1,14 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"github.com/bborbe/backup/dto"
 	"os"
 )
 
 type BackupService interface {
+	GetHost(host string) (dto.Host, error)
 	ListHosts() ([]dto.Host, error)
 	ListBackups(host dto.Host) ([]dto.Backup, error)
 	GetLatestBackup(host dto.Host) (dto.Backup, error)
@@ -56,9 +58,60 @@ func createHost(host string) dto.Host {
 }
 
 func (s *backupService) ListBackups(host dto.Host) ([]dto.Backup, error) {
-	return nil, nil
+	if host == nil {
+		return nil, errors.New("parameter host missing")
+	}
+	dir := fmt.Sprintf("%s%c%s", s.rootdir, os.PathSeparator, host.GetName())
+	file, err := os.Open(dir)
+	if err != nil {
+		return nil, err
+	}
+	fileinfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if !fileinfo.IsDir() {
+		return nil, fmt.Errorf("dir %s is not a directory", dir)
+	}
+	names, err := file.Readdirnames(0)
+	if err != nil {
+		return nil, err
+	}
+	return createBackups(names), nil
+}
+
+func createBackups(backups []string) []dto.Backup {
+	result := make([]dto.Backup, len(backups))
+	for i, backup := range backups {
+		result[i] = createBackup(backup)
+	}
+	return result
+}
+
+func createBackup(backup string) dto.Backup {
+	h := dto.NewBackup()
+	h.SetName(backup)
+	return h
 }
 
 func (s *backupService) GetLatestBackup(host dto.Host) (dto.Backup, error) {
 	return nil, nil
+}
+
+func (s *backupService) GetHost(host string) (dto.Host, error) {
+	dir := fmt.Sprintf("%s%c%s", s.rootdir, os.PathSeparator, host)
+	file, err := os.Open(dir)
+	if err != nil {
+		return nil, err
+	}
+	fileinfo, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if !fileinfo.IsDir() {
+		return nil, fmt.Errorf("dir %s is not a directory", dir)
+	}
+	h := dto.NewHost()
+	h.SetName(host)
+	return h, nil
 }
--- service/backup_service_test.go
+++ service/backup_service_test.go
@@ -30,6 +30,14 @@ func createHostDir(root string, host string) error {
 	return os.Mkdir(dir, fileMode)
 }
 
+func createBackupDir(root string, host string, backup string) error {
+	var fileMode os.FileMode
+	fileMode = 0777
+	dir := fmt.Sprintf("%s%c%s%c%s", root, os.PathSeparator, host, os.PathSeparator, backup)
+	logger.Debugf("create backupdir %s", dir)
+	return os.Mkdir(dir, fileMode)
+}
+
 func TestImplementsBackupService(t *testing.T) {
 	service := NewBackupService(BACKUP_ROOT_DIR)
 	var expected *BackupService
@@ -80,6 +88,84 @@ func TestListHosts(t *testing.T) {
 	}
 }
 
-func TestListBackups(t *testing.T) {
+func TestGetHost(t *testing.T) {
+	clearBackupRootDir(BACKUP_ROOT_DIR)
+	service := NewBackupService(BACKUP_ROOT_DIR)
+	hostName := "firewall.example.com"
+	{
+		_, err := service.GetHost(hostName)
+		err = AssertThat(err, NotNilValue().Message("expect error, backup dir not existing"))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	createBackupRootDir(BACKUP_ROOT_DIR)
+	createHostDir(BACKUP_ROOT_DIR, hostName)
+	{
+		host, err := service.GetHost(hostName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = AssertThat(host, NotNilValue())
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
 
+func TestListBackups(t *testing.T) {
+	clearBackupRootDir(BACKUP_ROOT_DIR)
+	service := NewBackupService(BACKUP_ROOT_DIR)
+	{
+		_, err := service.ListBackups(nil)
+		err = AssertThat(err, NotNilValue())
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	hostName := "firewall.example.com"
+	createBackupRootDir(BACKUP_ROOT_DIR)
+	createHostDir(BACKUP_ROOT_DIR, hostName)
+	{
+		host, err := service.GetHost(hostName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		backups, err := service.ListBackups(host)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = AssertThat(backups, NotNilValue())
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = AssertThat(len(backups), Is(0))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	backupName := "2013-07-28T00:24:52"
+	createBackupDir(BACKUP_ROOT_DIR, hostName, backupName)
+	{
+		host, err := service.GetHost(hostName)
+		if err != nil {
+			t.Fatal(err)
+		}
+		backups, err := service.ListBackups(host)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = AssertThat(backups, NotNilValue())
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = AssertThat(len(backups), Is(1))
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = AssertThat(backups[0].GetName(), Is(backupName))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
 }
