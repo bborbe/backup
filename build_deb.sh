c7efcc4e9a278ff959475ea7c6df10364e71f36e
Benjamin Borbe <bborbe@rocketnews.de>
2013-08-23 19:59:59 +0200

    add build deb script

---
+++ build_deb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf $GOPATH/package
+mkdir -p $GOPATH/package/DEBIAN
+mkdir -p $GOPATH/package/opt/backup/bin
+
+cp $GOPATH/src/github.com/bborbe/backup/control $GOPATH/package/DEBIAN/control
+cp $GOPATH/bin/backup_latest $GOPATH/package/opt/backup/bin/
+cp $GOPATH/bin/backup_list $GOPATH/package/opt/backup/bin/
+
+chmod 555 $GOPATH/package/opt/backup/bin/*
+dpkg -b $GOPATH/package $GOPATH/backup.deb
