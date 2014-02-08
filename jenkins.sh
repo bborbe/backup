8ac5fbb1498eac7d37989b99be5ad4054e068603
Benjamin Borbe <bborbe@rocketnews.de>
2014-02-08 16:02:39 +0100

    inline build_deb

--- jenkins.sh
+++ jenkins.sh
@@ -27,4 +27,58 @@ done
 
 go install github.com/bborbe/backup/bin/backup_cleanup github.com/bborbe/backup/bin/backup_latest github.com/bborbe/backup/bin/backup_list github.com/bborbe/backup/bin/backup_old github.com/bborbe/backup/bin/backup_keep
 sh src/github.com/bborbe/backup/build_deb.sh
+
+
+
+
+#########################################################################
+
+BINS="backup_cleanup backup_keep backup_latest backup_list backup_old"
+
+# Create scripts source dir
+DIR=$NAME-$VERSION
+echo $DIR
+rm -rf $DIR
+mkdir $DIR
+
+# Copy bins
+for BIN in $BINS
+do
+cp bin/$BIN $DIR/$BIN
+done
+cd $DIR
+
+# Create skeleton
+echo foo | DEBFULLNAME="Benjamin Borbe" dh_make --single --indep --createorig --copyright bsd --email bborbe@rocketnews.de
+
+# Remove make calls
+grep -v makefile debian/rules > debian/rules.new
+mv debian/rules.new debian/rules
+
+# Set distrubtion
+sed -i.bak 's/unstable/bborbe-unstable/g' debian/changelog
+
+# Add copyright
+cp ../src/github.com/bborbe/backup/LICENSE debian/copyright
+
+# Add to install
+for BIN in $BINS
+do
+echo $BIN opt/backup/bin | tee -a debian/install
+done
+
+# We don't want a quilt based package
+echo "1.0" > debian/source/format
+
+# Remove the example files
+rm debian/*.ex
+rm debian/README*
+
+# Build package
+debuild -us -uc
+
+#########################################################################
+
+
+
 dput -u misc.rn.benjamin-borbe.de ${NAME}_${VERSION}-1_amd64.changes
