0c34dfe819fef9fd4033f163b5634e689298c601
Benjamin Borbe <bborbe@rocketnews.de>
2014-03-11 13:48:15 +0100

    refactor server

--- bin/backup_status_server/backup_status_server.go
+++ bin/backup_status_server/backup_status_server.go
@@ -12,7 +12,6 @@ var logger = log.DefaultLogger
 const DEFAULT_PORT int = 8002
 
 func main() {
-	defer logger.Close()
 	logLevelPtr := flag.String("loglevel", log.LogLevelToString(config.DEFAULT_LOG_LEVEL), "one of OFF,TRACE,DEBUG,INFO,WARN,ERROR")
 	rootdirPtr := flag.String("rootdir", config.DEFAULT_ROOT_DIR, "root directory for backups")
 	portnumberPtr := flag.Int("port", DEFAULT_PORT, "server port")
@@ -23,20 +22,5 @@ func main() {
 	logger.Tracef("portnumberPtr %d", *portnumberPtr)
 	logger.Debugf("backup status server started at port %d", *portnumberPtr)
 	srv := status_server.NewServer(*portnumberPtr, *rootdirPtr)
-	{
-		err := srv.Start()
-		if err != nil {
-			logger.Errorf("start server failed, %v", err)
-			return
-		}
-	}
-	srv.Wait()
-	{
-		err := srv.Stop()
-		if err != nil {
-			logger.Errorf("stop server failed, %v", err)
-			return
-		}
-	}
-	logger.Debug("backup status server finished")
+	srv.Run()
 }
