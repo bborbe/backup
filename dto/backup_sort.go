a65621e7c0ebffccc5ec9038864c112311d32619
Benjamin Borbe <bborbe@rocketnews.de>
2014-10-08 17:59:16 +0200

    add resume

--- dto/backup_sort.go
+++ dto/backup_sort.go
@@ -6,6 +6,8 @@ import (
 
 type BackupByName []Backup
 
-func (v BackupByName) Len() int           { return len(v) }
-func (v BackupByName) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
-func (v BackupByName) Less(i, j int) bool { return stringutil.StringLess(v[i].GetName(), v[j].GetName()) }
+func (v BackupByName) Len() int      { return len(v) }
+func (v BackupByName) Swap(i, j int) { v[i], v[j] = v[j], v[i] }
+func (v BackupByName) Less(i, j int) bool {
+	return stringutil.StringLess(v[i].GetName(), v[j].GetName())
+}
