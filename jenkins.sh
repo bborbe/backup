a4ec5fe68f930df5172a2e41a96cbcd9f4edbe0b
Benjamin Borbe <bborbe@rocketnews.de>
2014-10-13 14:06:01 +0200

    add resume to package

--- jenkins.sh
+++ jenkins.sh
@@ -8,8 +8,8 @@ export DEB_SERVER=misc.rn.benjamin-borbe.de
 export TARGET_DIR=opt/backup/bin
 
 export NAME=backup
-export BINS="backup_cleanup backup_keep backup_latest backup_list backup_old"
-export INSTALLS="github.com/bborbe/backup/bin/backup_cleanup github.com/bborbe/backup/bin/backup_latest github.com/bborbe/backup/bin/backup_list github.com/bborbe/backup/bin/backup_old github.com/bborbe/backup/bin/backup_keep"
+export BINS="backup_cleanup backup_keep backup_latest backup_list backup_old backup_resume"
+export INSTALLS="github.com/bborbe/backup/bin/backup_cleanup github.com/bborbe/backup/bin/backup_keep github.com/bborbe/backup/bin/backup_latest github.com/bborbe/backup/bin/backup_list github.com/bborbe/backup/bin/backup_old github.com/bborbe/backup/bin/backup_resume"
 export SOURCEDIRECTORY="github.com/bborbe/backup"
 
 export MAJOR=0
