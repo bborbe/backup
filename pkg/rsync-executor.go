6185015c3f411c693f04297f15c109afccc4cb19
Benjamin Borbe <benjamin.borbe@gmail.com>
2024-08-25 16:24:28 +0200

    print rsync output by default

--- pkg/rsync-executor.go
+++ pkg/rsync-executor.go
@@ -25,7 +25,7 @@ type rsyncExectuor struct {
 func (r *rsyncExectuor) Rsync(ctx context.Context, args ...string) error {
 	glog.V(2).Infof("run: rsync %s", strings.Join(args, " "))
 	cmd := exec.Command("rsync", args...)
-	if glog.V(4) {
+	if glog.V(2) {
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = os.Stdout
 	}
