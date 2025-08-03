88504b892a461b077439bce41cb24bce53ed8480
Benjamin Borbe <benjamin.borbe@gmail.com>
2025-08-03 15:47:35 +0200

    remove vi from imports

--- frontend/src/tests/ActionButtonComponent.test.ts
+++ frontend/src/tests/ActionButtonComponent.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import ActionButtonComponent from '../components/ActionButtonComponent.vue';
 
